26582641ed6958576df98018b2f4c9bfd2991cef
Hiléo Andersson <hileomsi@gmail.com>
2020-12-18 10:47:31 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Release Notes Format
+
+Format your release notes before creating the release.
+
+## Pre-requisites
+
+Create a workflow .yml file in your .github/workflows directory. An example workflow is available below. For more information, reference the GitHub Help Documentation for Creating a workflow file.
+
+## Inputs
+
+`github-token`: Github token. (**required**)
+
+`owner`: Repository owner. (**required**)
+
+`repo`: Repository name. (**required**)
+
+`milestone`: Milesone id, not title please. (**required**)
+
+`custom-row`: Custom format to row. You can use [issue](https://docs.github.com/en/free-pro-team@latest/rest/reference/issues#get-an-issue) values ​​to build your row. Example: "(#${issue.number}) - ${issue.title}: ${issue.body}"
+
+`custom-group-by-label`: Custom groups by label.
+
+## Outputs
+
+`releaseNotes`: Formatted release notes.
+
+## Example
+
+```yaml
+name: Production Deploy
+
+on:
+  workflow_dispatch:
+    inputs:
+      milestone:
+        description: "Milestone ID"
+        required: true
+
+jobs:
+  production:
+    name: Deploy Production
+    runs-on: ubuntu-latest
+    steps:
+      - name: Relese notes format
+        uses: Beakyn/release-notes-format@master
+        id: format
+        with:
+          github-token: ${{ github.token }}
+
+          owner: "repository_owner"
+          repo: "repository_name"
+          milestone: ${{ github.event.inputs.milestone }}
+
+          # optional
+          custom-row: "(#${issue.number}) - ${issue.title}: ${issue.body}"
+          custom-group-by-label: |
+            [
+              {
+                title: '### Feature 🎉\n\n',
+                labels: ['feature'],
+              },
+              {
+                title: '\n### Bug Fixes 🐛\n\n',
+                labels: ['bug'],
+              },
+              {
+                title: '\n### Refactor & Improvements ✨\n\n',
+                labels: ['enhancement', 'refactor', 'chore'],
+              }
+            ]
+
+      - name: Create Release
+        id: create_release
+        uses: actions/create-release@v1
+        env:
+          GITHUB_TOKEN: ${{ github.token }}
+        with:
+          tag_name: ${{ github.ref }}
+          release_name: Release ${{ github.ref }}
+          body: ${{steps.format.releaseNotes}}
+          draft: false
+          prerelease: false
+
+      - name: Close Milestone
+        uses: WyriHaximus/github-action-close-milestone
+        with:
+          number: ${{ github.event.inputs.milestone }}
+```
---
+++ index.js
@@ -0,0 +1,109 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const template = require('lodash.template');
+
+const defaultRow = '- ${issue.title} #${issue.number}\n';
+const defaultGroupByLabel = [
+  {
+    title: '### Feature 🎉\n\n',
+    labels: ['feature'],
+  },
+  {
+    title: '\n### Bug Fixes 🐛\n\n',
+    labels: ['bug'],
+  },
+  {
+    title: '\n### Refactor & Improvements ✨\n\n',
+    labels: ['enhancement', 'refactor', 'chore'],
+  }
+];
+
+function getInputs() {
+  const requiredOptions = { required: true };
+  // Required
+  const tokenGithub = core.getInput('github-token', requiredOptions);
+  const owner = core.getInput('owner', requiredOptions);
+  const repo = core.getInput('repo', requiredOptions);
+  const milestone = core.getInput('milestone', requiredOptions);
+  // Optional
+  const customRow = core.getInput('custom-row');
+  const groupByLabel = core.getInput('custom-group-by-label');
+
+  return {
+    tokenGithub,
+    owner,
+    repo,
+    milestone,
+    row: customRow || defaultRow,
+    groupByLabel: groupByLabel === '' ? defaultGroupByLabel : JSON.parse(groupByLabel)
+  };
+}
+
+function concatText(previous, current) {
+  previous += current;
+  return previous;
+};
+
+async function run() {
+  try {
+    const {
+      tokenGithub,
+      owner,
+      repo,
+      milestone,
+      row,
+      groupByLabel,
+    } = getInputs();
+
+    const octokit = github.getOctokit(tokenGithub);
+  
+    const { data: issues } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+      owner,
+      repo,
+      milestone,
+      state: 'closed',
+      sort: 'created',
+      direction: 'asc'
+    });
+  
+    const releaseNotes = groupByLabel.map(item => {
+      // filtering by group
+      const changeLog = issues
+      // removing pull_request issues
+      .filter(issue => !issue.pull_request)
+      // checking the label
+      .filter(issue => {
+        const exists = issue.labels.some(label => {
+          return item.labels.includes(label.name);
+        });
+
+        return exists;
+      })
+      // creating each issue text
+      .map(issue => template(row)({ issue }))
+      // concatenating the title of the issues
+      .reduce(concatText, '');
+
+      return {
+        title: item.title,
+        changeLog
+      }
+    })
+    // removing groups that have no change log
+    .map(({ title, changeLog }) => {
+      return (changeLog.trim() !== '')
+        ? `${title}${changeLog}`
+        : '';
+    })
+    // concatenating change log groups
+    .reduce(concatText, '');
+    // success
+    console.log(releaseNotes);
+    core.setOutput('releaseNotes', releaseNotes);
+  } catch(error) {
+    console.log('error => ', error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
